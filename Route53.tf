eb6e4afe1c9ca7be03aab786aca138aba7737461
edeno20102 <167296019+edeno20102@users.noreply.github.com>
2024-12-23 09:25:53 +0200

    changes type

--- Route53.tf
+++ Route53.tf
@@ -10,7 +10,10 @@ resource "aws_route53_zone" "main" {
 resource "aws_route53_record" "web" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "test.365eden.com"
-  type    = "CNAME"
-  ttl     = 300
-  records = [aws_lb.web.dns_name]
+  type    = "A"
+  alias {
+    name                   = aws_lb.web.dns_name
+    zone_id                = aws_lb.web.zone_id
+    evaluate_target_health = true
+  }
 }
